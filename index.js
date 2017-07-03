e2c619279f84b8431898f735d3151362f0e51dd7
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-07-03 16:27:22 +0200

    Revert code style change

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ module.exports = user => {
 		throw new TypeError(`Expected a string, got ${typeof user}`);
 	}
 
-	return got(`https://instagram.com/${user}/?__a=1`, {
-		json: true
-	}).then(res => ({
+	return got(`https://instagram.com/${user}/?__a=1`, {json: true}).then(res => ({
 		description: res.body.user.biography || '',
 		email: getEmails(res.body.user.biography || '')[0] || '',
 		followers: res.body.user.followed_by.count,
