ebf49af167d8ee9f76b990b363c603f09a1af83e
Kevin Martensson <kevinmartensson@gmail.com>
2019-09-17 10:52:17 +0200

    Adjust for new response body structure from Instagram
    Fixes #3.

--- index.js
+++ index.js
@@ -2,29 +2,34 @@
 const getEmails = require('get-emails');
 const got = require('got');
 
-module.exports = user => {
-	if (typeof user !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof user}`);
+module.exports = username => {
+	if (typeof username !== 'string') {
+		throw new TypeError(`Expected a string, got ${typeof username}`);
 	}
 
-	return got(`https://instagram.com/${user}/?__a=1`, {json: true})
-		.then(res => ({
-			description: res.body.user.biography || '',
-			email: getEmails(res.body.user.biography || '').values().next().value || '',
-			followers: res.body.user.followed_by.count,
-			following: res.body.user.follows.count,
-			fullName: res.body.user.full_name || '',
-			id: res.body.user.id,
-			posts: res.body.user.media.count,
-			url: `https://instagram.com/${user}`,
-			username: res.body.user.username,
-			website: res.body.user.external_url || ''
-		}))
-		.catch(err => {
-			if (err.statusCode === 404) {
-				err.message = `User "${user}" not found`;
+	return got(`https://instagram.com/${username}/?__a=1`, {json: true})
+		.then(response => {
+			const user = response.body.graphql.user;
+			const email = getEmails(user.biography || '').values().next().value;
+
+			return ({
+				description: user.biography || '',
+				email: email || '',
+				followers: user.edge_followed_by.count,
+				following: user.edge_follow.count,
+				fullName: user.full_name || '',
+				id: user.id,
+				posts: user.edge_owner_to_timeline_media.count,
+				url: `https://instagram.com/${username}`,
+				username,
+				website: user.external_url || ''
+			});
+		})
+		.catch(error => {
+			if (error.statusCode === 404) {
+				error.message = `User "${username}" not found`;
 			}
 
-			throw err;
+			throw error;
 		});
 };
